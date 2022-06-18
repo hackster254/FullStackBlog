11cd2b38f5504b4dd063b0fb795fac0cd1ebf3ad
karira <hackster.origin@gmail.com>
2022-06-18 13:21:12 +0300

    changed port to 5000

--- nodejsbackend/server.js
+++ nodejsbackend/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 5000
 
 const cors = require('cors')
 require('./connection')
